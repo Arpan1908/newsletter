2f194bfd03d40798161697caa115b8eb76c81fc2
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2023-12-12 00:07:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ app.use(bodyParser.json());
 
 // CORS handling - replace with your frontend's URL
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://newsletter-zia2.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
